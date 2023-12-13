3c2335a73df0aace1a0b48463254c8931911cb1d
HELEN-DEE <helendeee12@gmail.com>
2023-12-14 00:27:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+import data from './data.json'
+
+const dashboardData = data
+console.log(dashboardData)
+
+// fetch('./data.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
